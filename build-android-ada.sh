4e6dea9d21dd9360e52946909bb5960630166d88
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-03-25 21:59:22 +0300

    Switch to llvm-gnatlink from ld.lld

--- build-android-ada.sh
+++ build-android-ada.sh
@@ -6,7 +6,7 @@ SYSROOT=$CLANG_R475365B_CUSTOM/../../../sysroots/ndk/arm64
 
 mkdir -p android
 $CLANG_R475365B_CUSTOM/bin/clang --target=aarch64-linux-android29 -ggdb main.c -o android/main -ldl --sysroot=$SYSROOT
-$LLVM_GNAT/llvm-interface/bin/llvm-gcc --target=aarch64-linux-android29 -ggdb -f -fPIC -gnat2022 -c message.adb
+$LLVM_GNAT/llvm-interface/bin/llvm-gcc --target=aarch64-linux-android29 -ggdb -fPIC -gnat2022 -c message.adb
 $LLVM_GNAT/llvm-interface/bin/llvm-gnatbind -Lmessage message.ali
-#$LLVM_GNAT/llvm-interface/bin/llvm-gnatlink message.ali -o android/libmessage.so -shared
-$CLANG_R475365B_CUSTOM/bin/ld.lld -shared message.o -o android/libmessage.so
+$LLVM_GNAT/llvm-interface/bin/llvm-gnatlink message.ali -o android/libmessage.so -shared
+#$CLANG_R475365B_CUSTOM/bin/ld.lld -shared message.o -o android/libmessage.so
