43277641819da8539efdefcf0550a5aa9cad665f
anonymix007 <48598263+anonymix007@users.noreply.github.com>
2024-03-25 22:47:11 +0300

    Force gnatlink to use lld and adjust library paths

--- build-android-ada.sh
+++ build-android-ada.sh
@@ -3,10 +3,11 @@
 set -xe
 
 SYSROOT=$CLANG_R475365B_CUSTOM/../../../sysroots/ndk/arm64
+LLD=$CLANG_R475365B_CUSTOM/bin/ld.lld
 
 mkdir -p android
 $CLANG_R475365B_CUSTOM/bin/clang --target=aarch64-linux-android29 -ggdb main.c -o android/main -ldl --sysroot=$SYSROOT
 $LLVM_GNAT/llvm-interface/bin/llvm-gcc --target=aarch64-linux-android29 -ggdb -fPIC -gnat2022 -c message.adb
 $LLVM_GNAT/llvm-interface/bin/llvm-gnatbind -Lmessage message.ali
-$LLVM_GNAT/llvm-interface/bin/llvm-gnatlink message.ali -o android/libmessage.so -shared
+$LLVM_GNAT/llvm-interface/bin/llvm-gnatlink message.ali -o android/libmessage.so --LINK=$LLD -shared --sysroot=$SYSROOT -L$SYSROOT/usr/lib/aarch64-linux-android
 #$CLANG_R475365B_CUSTOM/bin/ld.lld -shared message.o -o android/libmessage.so
